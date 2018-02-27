75430c20b150e36045c78d94488583cc5b733806
Callum Osborne <callyozzie@hotmail.co.uk>
2018-02-27 14:58:54 +0000

    Updated Tabs

--- src/GUI.java
+++ src/GUI.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 
 public class GUI {
     private JTabbedPane TabbedPannel;
-    private JTextField helloWorldTextField;
     private JPanel PanelMain;
     private JPanel pnlMainMenu;
     private JPanel pnlMyStats;
     private JPanel pnlSettings;
+    private JButton btnStart;
+    private JComboBox lstCatergory;
+    private JCheckBox chkEnableNotifications;
+    private JButton btnSchedule;
+    private JLabel lbl1;
+    private JLabel lbl2;
 
     public GUI() {
 
@@ -15,8 +20,9 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setTitle("ProjectX");
 
-        frame.setSize(800,720);
+        frame.setSize(600,500);
 
     }
 
