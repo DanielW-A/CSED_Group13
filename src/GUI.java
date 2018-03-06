2c69cf87c6058923e21f5f10d03bcf8164c23dec
Callum Osborne <callyozzie@hotmail.co.uk>
2018-03-06 12:46:03 +0000

    Alterations to 'Start' button
    During 06/03/2018 meeting.

--- src/GUI.java
+++ src/GUI.java
@@ -26,20 +26,25 @@ public class GUI {
 
         frame.setSize(600,500);
 
+        //Button Code
         btnStart.addActionListener(new ActionListener() {
             long startTime;
             long endTime;
             public void actionPerformed(ActionEvent e) {
+                //When starting
                 if (btnStart.getText().equals("Start")){
                     btnStart.setText("Stop");
                     startTime = System.currentTimeMillis();
                     System.out.println("Start Time: " + startTime);
+                    lstCatergory.setEnabled(false);
                 }
+                //When stopping
                 else{
                     btnStart.setText("Start");
                     endTime = System.currentTimeMillis();
                     System.out.println("End Time: " + endTime);
                     System.out.println("Change in time: " + (endTime - startTime));
+                    lstCatergory.setEnabled(true);
                 }
 
 
